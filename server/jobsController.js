1c0897558a380f7c30f65f9284f8d86b85e0d398
Cj <carterjpilling@gmail.com>
2020-11-23 15:05:44 -0700

    Added modal to jobs

--- server/jobsController.js
+++ server/jobsController.js
@@ -2,16 +2,16 @@ module.exports = {
   saveJobs: async (req, res) => {
     const db = req.app.get('db')
     const { id } = req.session.user
-    const { date, job_status, job_name, job_link, job_notes } = req.body
+    const { date, job_status, job_name, job_company, job_link, job_notes } = req.body
     const [existingDate] = await db.check_date([date])
 
     if (existingDate) {
-      await db.post_jobs([id, existingDate.id, job_status, job_name, job_link, job_notes])
+      await db.post_jobs([id, existingDate.id, job_status, job_name, job_company, job_link, job_notes])
     }
 
     if (!existingDate) {
       const [newDate] = await db.create_date([date])
-      await db.post_jobs([id, newDate.id, job_status, job_name, job_link, job_notes])
+      await db.post_jobs([id, newDate.id, job_status, job_name, job_company, job_link, job_notes])
     }
 
     res.sendStatus(200)
--- src/Components/Jobs.js
+++ src/Components/Jobs.js
@@ -1,16 +1,54 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { connect } from 'react-redux'
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function Jobs(props) {
-
+  const classes = useStyles();
   const [jobs, setJobs] = useState([])
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+  const [statusState, setStatusState] = useState('')
+  const [nameState, setNameState] = useState('')
+  const [linkState, setLinkState] = useState('')
+  const [noteState, setNoteState] = useState('')
+  const [companyState, setCompanyState] = useState('')
   useEffect(() => {
     if (props.isLoggedIn) {
       console.log('Job Function')
       getJobs()
     }
   }, [props.date, props.isLoggedIn])
+
+
+
 
   function getJobs() {
     setJobs([])
@@ -20,21 +58,74 @@ function Jobs(props) {
   }
 
   function saveJob() {
+    axios.post('/api/user/jobs', {
+      date: props.date,
+      job_status: statusState,
+      job_name: nameState,
+      job_company: companyState,
+      job_link: linkState,
+      job_notes: noteState
+    })
+      .then(() => getJobs())
+  }
+
+  function editJob(id) {
+    //I can either get the id from the jobs state or get it from the backend.... 
+
+  }
+  function saveEdit(id) {
+    axios.put(`/api/users/jobs/${id}`, {
+      date: props.date,
+      job_status: statusState,
+      job_name: nameState,
+      job_company: companyState,
+      job_link: linkState,
+      job_notes: noteState
+    })
+      .then(() => getJobs())
 
   }
 
-  function editJob() {
+  function deleteJob(id) {
+    axios.delete(`/api/users/jobs/${id}`)
+      .then(() => getJobs())
+      .catch((err) => console.log(err))
 
   }
 
-  function deleteJob() {
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-  }
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">Text in a modal</h2>
+      <p id="simple-modal-description">
+        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+      </p>
+      <input placeholder='Job Name' />
+      <input placeholder='Job ' />
+    </div>
+  );
 
   return (
-    <>
-      <p>Jobs</p>
-    </>
+    <div>
+      <button type="button" onClick={handleOpen}>
+        + New Job
+      </button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+    </div>
   )
 }
 
